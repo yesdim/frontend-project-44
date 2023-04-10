21fb59f6aa66459b23a1e11915d3785f23f0aa73
nodim <nodim@nodim.com>
2023-04-10 19:50:13 +0300

    add index.js

--- src/brainEven.js
+++ src/brainEven.js
@@ -1,23 +1,23 @@
-import readlineSync from 'readline-sync';
-import brainGame from './cli.js';
+import gameLogics from './index.js';
 
 function num() {
   const number = (Math.round(Math.random() * 100));
   return number;
 }
 
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const questionInGame = num();
+let rightAnswer;
+
 export default function brainEven() {
-  const name = brainGame();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const numsave = num();
-    const quest = readlineSync.question(`${'Question: '}${numsave}${'\nYour answer: '}`);
-    if ((numsave % 2 === 0 && quest === 'yes') || (numsave % 2 !== 0 && quest === 'no')) {
-      console.log('Coorect!');
-    } else {
-      console.log(`${quest} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-      return;
-    }
+  if ((questionInGame % 2 === 0)) {
+    rightAnswer = 'yes';
+  } else {
+    rightAnswer = 'no';
   }
-  console.log(`Congratulations, ${name}!`);
+  return rightAnswer;
 }
+
+const correctAnswer = brainEven();
+
+gameLogics(rules, questionInGame, correctAnswer);
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import readlineSync from 'readline-sync';
+import brainGame from './cli.js';
+
+export default function gameLogics(rules, questionInGame, correctAnswer) {
+    const name = brainGame();
+    console.log(rules);
+    for (let i = 0; i < 3; i += 1) {
+        const quest = readlineSync.question(`${'Question: '}${questionInGame}${'\nYour answer: '}`);
+        if (quest === correctAnswer) {
+            console.log('Coorect!');
+        } else {
+            console.log(`${quest} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`);
+            return;
+        }
+    }
+    console.log(`Congratulations, ${name}!`);
+}
